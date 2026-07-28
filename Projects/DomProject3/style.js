53a5a4c2689333dd0f7199385813e39b0710f0c3
Shiva Pradhan <shivapradhan.cybersec@gmail.com>
2026-07-28 07:31:33 +0545

    added project3

---
+++ Projects/DomProject3/style.js
@@ -0,0 +1,8 @@
+let cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", function(dets){
+
+    cursor.style.left = dets.x + "px";
+    cursor.style.top = dets.y + "px";
+
+});
