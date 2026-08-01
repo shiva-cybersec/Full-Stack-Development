f16c3789d9794c8abf1bfc760e584752f91bf7b1
Shiva Pradhan <shivapradhan.cybersec@gmail.com>
2026-08-01 18:36:37 +0545

    project13

--- Projects/DomProject13/script.js
+++ Projects/DomProject13/script.js
@@ -1,3 +1,8 @@
-addEventListener("mouse",(e)=>{
-    console.log("mouse moving")
+addEventListener("mousemove",(e)=>{
+    console.log(e.clientX, e.clientY)    
+
+    document.body.style.setProperty("--x", e.clientX + "px")
+    document.body.style.setProperty("--y", e.clientY + "px")
 })
+
+
